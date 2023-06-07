02c9f66d400118bf0cfd0624ec17f424dc323aa8
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-06-07 13:52:31 -0500

    first commit

---
+++ src/main/java/com/soundconnect/soundconnect/SoundConnectApplication.java
@@ -0,0 +1,13 @@
+package com.soundconnect.soundconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundConnectApplication.class, args);
+    }
+
+}
