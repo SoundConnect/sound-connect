c3000b19dd1b82df6ea605c4f0fddf7313a5fa1f
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-06-09 14:48:41 -0500

    fixed nav bar

--- src/main/java/com/soundconnect/soundconnect/controller/PlaylistController.java
+++ src/main/java/com/soundconnect/soundconnect/controller/PlaylistController.java
@@ -2,6 +2,7 @@ package com.soundconnect.soundconnect.controller;
 
 import com.soundconnect.soundconnect.model.Playlist;
 import com.soundconnect.soundconnect.repositories.PlaylistRepository;
+import com.soundconnect.soundconnect.repositories.TrackRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +13,12 @@ import java.util.List;
 @Controller
 public class PlaylistController {
 
-    private final PlaylistRepository playlistDao;
+    private final PlaylistRepository playlistsDao;
+    private final TrackRepository tracksDao;
 
-    public PlaylistController(PlaylistRepository playlistDao) {
-        this.playlistDao = playlistDao;
+    public PlaylistController(PlaylistRepository playlistsDao, TrackRepository tracksDao) {
+        this.playlistsDao = playlistsDao;
+        this.tracksDao = tracksDao;
     }
 
     // show form for creating a playlist
@@ -27,13 +30,14 @@ public class PlaylistController {
     // get form data and create playlist
     @PostMapping("create")
     public String createPlaylist(){
+
         return "redirect:/profile";
     }
 
     // show feed for all shared playlists
     @GetMapping("/feed")
     public String showFeed(Model model){
-        List<Playlist> playlists = playlistDao.findAll();
+        List<Playlist> playlists = playlistsDao.findAll();
         model.addAttribute("playlists", playlists);
         return "feed";
     }
