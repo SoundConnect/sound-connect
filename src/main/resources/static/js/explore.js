d05409ad40bec81b1bacf044ba3230b8319787b0
Damian Wrather <damian.wrather@gmail.com>
2023-06-10 14:42:41 -0500

    music buttons added and endpoints updated

--- src/main/resources/static/js/explore.js
+++ src/main/resources/static/js/explore.js
@@ -4,6 +4,14 @@ let token = await getToken();
 let artistToken = await getToken();
 let albumToken = await getToken();
 
+// spotify-player.js
+
+// Initialize the player
+
+
+// Function to retrieve the Spotify access token
+
+
 async function displayPlaylist(token) {
     try {
         const response = await fetch('https://api.spotify.com/v1/recommendations?limit=5&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry&seed_tracks=0c6xIDDpzE81m2q797ordA', {
@@ -17,7 +25,7 @@ async function displayPlaylist(token) {
         const playlistContainer = document.querySelector('.explore-playlist');
         const playlists = data.tracks;
 
-        playlists.forEach(playlist => {
+        playlists.forEach((playlist, index) => {
             const playlistRow = document.createElement('div');
             playlistRow.className = 'row playlist-row';
 
@@ -50,16 +58,65 @@ async function displayPlaylist(token) {
             playlistRow.appendChild(column1);
             playlistRow.appendChild(column2);
 
+            // Add the song row within the playlist row
+            const songRow = document.createElement('div');
+            songRow.className = 'song-row';
+            songRow.id = `song${index + 1}`;
+
+            const songTitle = document.createElement('span');
+            songTitle.textContent = playlist.name;
+
+            const playButton = document.createElement('button');
+            playButton.className = 'play-button';
+            playButton.textContent = 'Play';
+
+            songRow.appendChild(songTitle);
+            songRow.appendChild(playButton);
+
+            playlistRow.appendChild(songRow);
+
             playlistContainer.appendChild(playlistRow);
         });
+
+        // Add event listeners to the play buttons
+        const playButtons = document.getElementsByClassName('play-button');
+        Array.from(playButtons).forEach((button, index) => {
+            button.addEventListener('click', () => {
+                const songId = `song${index + 1}`; // Get the song ID based on the button index
+                const songRow = document.getElementById(songId);
+                const songTitle = songRow.querySelector('span').textContent;
+                const trackUri = fetchTrackUri(songTitle); // Call a function to retrieve the track URI or ID for the song
+                playSong(trackUri);
+            });
+        });
+
     } catch (error) {
         console.error('Error:', error);
     }
 }
+
+// Play a specific song using the Spotify Player SDK
+function playSong(trackUri) {
+    player.resume().then(() => {
+        player.play({
+            uris: [trackUri]
+        });
+    });
+}
+
+// Call a function to retrieve the track URI or ID for the song
+function fetchTrackUri(songTitle) {
+    // Add your code to fetch the track URI or ID for the given song title
+    // This can involve making API calls to the Spotify Web API or any other method of obtaining the track information
+    // Return the track URI or ID
+    return 'TRACK_URI_OR_ID';
+}
+
+
 async function getArtists(artistToken) {
 
     try {
-        const response = await fetch('https://api.spotify.com/v1/artists?ids=2CIMQHirSU0MQqyYHq0eOx%2C57dN52uHvrHOxijzpIgu3E%2C1vCWHaC5f2uS3yhpwWbIA6', {
+        const response = await fetch('https://api.spotify.com/v1/recommendations?limit=5&seed_artists=3TVXtAsR1Inumwj472S9r4&seed_genres=hip-hop%2Cclassical%2Ccountry&seed_tracks=59nOXPmaKlBfGMDeOVGrIK', {
                 method: 'GET',
                 headers: {
                     'Authorization': 'Bearer ' + token
@@ -68,8 +125,9 @@ async function getArtists(artistToken) {
         );
 
         const data = await response.json();
+        console.log(data)
         const artistContainer = document.querySelector('.explore-artists');
-        const artists = data.artists;
+        const artists = data.tracks;
         artists.forEach(artist => {
             const artistRow = document.createElement('div');
             artistRow.className = 'row artist-row';
@@ -79,7 +137,7 @@ async function getArtists(artistToken) {
 
             const artistImage = document.createElement('img');
             artistImage.className = 'artist-image';
-            artistImage.src = artist.images[0].url;
+            artistImage.src = artist.album.images[0].url;
             artistImage.alt = 'artist image';
             column1.appendChild(artistImage);
 
@@ -89,14 +147,14 @@ async function getArtists(artistToken) {
             const artistInfo = document.createElement('div');
             artistInfo.className = 'artist-info';
 
-            const artistName = document.createElement('h1');
-            artistName.textContent = artist.name;
+            // const artistName = document.createElement('h1');
+            // artistName.textContent = artist.name;
 
             const artistGenres = document.createElement('h2');
-            artistGenres.className = 'artist-genres';
-            artistGenres.textContent = artist.genres.slice(0, 1).join(', ');
+            artistGenres.className = 'artist-name';
+            artistGenres.textContent = artist.artists[0].name
 
-            artistInfo.appendChild(artistName);
+            // artistInfo.appendChild(artistName);
             artistInfo.appendChild(artistGenres);
 
             column2.appendChild(artistInfo);
@@ -169,4 +227,18 @@ async function getAlbums(token) {
     let playlist = await displayPlaylist(token);
     let artists = await getArtists(artistToken);
     let albums = await getAlbums(albumToken);
+    window.onSpotifyWebPlaybackSDKReady = () => {
+        const player = new Spotify.Player({
+            name: 'My Web Player',
+            getOAuthToken: async callback => {
+                // Call a function to retrieve the Spotify access token
+                const accessToken = await getToken();
+                callback(accessToken);
+            },
+            volume: 0.5 // Adjust the volume as desired
+        });
+
+        // Connect to the player
+        player.connect();
+    };
 })();
