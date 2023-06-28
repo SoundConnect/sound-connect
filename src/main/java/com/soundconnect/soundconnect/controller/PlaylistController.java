bbc33e5db43349139ef45b175432d530bc9282d6
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-06-27 20:41:29 -0500

    Update Navbar- footer-login-register-pgs

--- src/main/java/com/soundconnect/soundconnect/controller/PlaylistController.java
+++ src/main/java/com/soundconnect/soundconnect/controller/PlaylistController.java
@@ -3,14 +3,13 @@ package com.soundconnect.soundconnect.controller;
 import com.soundconnect.soundconnect.model.*;
 
 import com.soundconnect.soundconnect.repositories.*;
-
-import java.util.HashSet;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
